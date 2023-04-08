a72ceb606bc1f534fc24e85bddf25e18c1bd00d4
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-08 18:17:12 -0400

    Add styling to the aside links

--- src/Components/Aside.js
+++ src/Components/Aside.js
@@ -15,7 +15,10 @@ import {
 export default function Aside() {
   return (
     <div className="bg-primary flex flex-col text-[white] text-xl font-bold list-none pt-10 pl-5 w-[250px] h-screen justify-start gap-y-20">
-      <Link className="flex text-3xl items-center gap-2 hover:underline underline-offset-8 decoration-secondary  " to={"/"}>
+      <Link
+        className="flex text-3xl items-center gap-2 hover:underline underline-offset-8 decoration-secondary  "
+        to={"/"}
+      >
         <span>
           <MdDashboard color="white" size={30} />
         </span>
@@ -23,17 +26,62 @@ export default function Aside() {
       </Link>
 
       <div className="flex flex-col gap-y-4">
-        <Link className="flex items-center gap-4" to={"/"}><span><MdHome /></span>Home</Link>
-        <Link className="flex items-center gap-4" to={"/"}><span><MdAccountBox /></span>Profile</Link>
-        <Link className="flex items-center gap-4" to={"/"}><span><MdModeComment /></span>Messages</Link>
-        <Link className="flex items-center gap-4" to={"/"}><span><MdSchedule className="" color="white"/></span>History</Link>
-        <Link className="flex items-center gap-4" to={"/"}><span><MdPendingActions /></span>Tasks</Link>
-        <Link className="flex items-center gap-4" to={"/"}><span><MdGroups /></span>Communities</Link>
+        <Link className="flex items-center gap-4 hover:underline underline-offset-8 decoration-secondary" to={"/"}>
+          <span>
+            <MdHome />
+          </span>
+          Home
+        </Link>
+        <Link className="flex items-center gap-4 hover:underline underline-offset-8 decoration-secondary" to={"/"}>
+          <span>
+            <MdAccountBox />
+          </span>
+          Profile
+        </Link>
+        <Link className="flex items-center gap-4 hover:underline underline-offset-8 decoration-secondary" to={"/"}>
+          <span>
+            <MdModeComment />
+          </span>
+          Messages
+        </Link>
+        <Link className="flex items-center gap-4 hover:underline underline-offset-8 decoration-secondary" to={"/"}>
+          <span>
+            <MdSchedule className="" color="white" />
+          </span>
+          History
+        </Link>
+        <Link className="flex items-center gap-4 hover:underline underline-offset-8 decoration-secondary" to={"/"}>
+          <span>
+            <MdPendingActions />
+          </span>
+          Tasks
+        </Link>
+        <Link className="flex items-center gap-4 hover:underline underline-offset-8 decoration-secondary" to={"/"}>
+          <span>
+            <MdGroups />
+          </span>
+          Communities
+        </Link>
       </div>
       <div className="flex flex-col gap-y-4">
-        <Link className="flex items-center gap-4" to={"/"}><span><MdOutlineSettings /></span>Settings</Link>
-        <Link className="flex items-center gap-4" to={"/"}><span><MdHelpCenter /></span>Support</Link>
-        <Link className="flex items-center gap-4" to={"/"}><span><MdPrivacyTip /></span>Privacy</Link>
+        <Link className="flex items-center gap-4 hover:underline underline-offset-8 decoration-secondary" to={"/"}>
+          <span>
+            <MdOutlineSettings />
+          </span>
+          Settings
+        </Link>
+        <Link className="flex items-center gap-4 hover:underline underline-offset-8 decoration-secondary" to={"/"}>
+          <span>
+            <MdHelpCenter />
+          </span>
+          Support
+        </Link>
+        <Link className="flex items-center gap-4 hover:underline underline-offset-8 decoration-secondary" to={"/"}>
+          <span>
+            <MdPrivacyTip />
+          </span>
+          Privacy
+        </Link>
       </div>
     </div>
   );
