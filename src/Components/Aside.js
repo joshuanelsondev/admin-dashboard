14477610ebd313fc025f4bc9f565941e09792907
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-08 00:34:47 -0400

    Edit Aside

--- src/Components/Aside.js
+++ src/Components/Aside.js
@@ -1,7 +1,40 @@
+import { Link } from "react-router-dom";
+import {
+  MdDashboard,
+  MdHome,
+  MdSchedule,
+  MdModeComment,
+  MdGroups,
+  MdAccountBox,
+  MdOutlineSettings,
+  MdPrivacyTip,
+  MdHelpCenter,
+  MdPendingActions,
+} from "react-icons/md";
+
 export default function Aside() {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
+  return (
+    <div className="bg-primary flex flex-col text-[white] text-xl font-bold list-none pt-10 pl-5 w-[250px] h-screen justify-start gap-y-20">
+      <Link className="flex text-3xl items-center gap-2 hover:underline underline-offset-8 decoration-secondary  " to={"/"}>
+        <span>
+          <MdDashboard color="white" size={30} />
+        </span>
+        Dashboard
+      </Link>
+
+      <div className="flex flex-col gap-y-4">
+        <Link className="flex items-center gap-4" to={"/"}><span><MdHome /></span>Home</Link>
+        <Link className="flex items-center gap-4" to={"/"}><span><MdAccountBox /></span>Profile</Link>
+        <Link className="flex items-center gap-4" to={"/"}><span><MdModeComment /></span>Messages</Link>
+        <Link className="flex items-center gap-4" to={"/"}><span><MdSchedule className="" color="white"/></span>History</Link>
+        <Link className="flex items-center gap-4" to={"/"}><span><MdPendingActions /></span>Tasks</Link>
+        <Link className="flex items-center gap-4" to={"/"}><span><MdGroups /></span>Communities</Link>
+      </div>
+      <div className="flex flex-col gap-y-4">
+        <Link className="flex items-center gap-4" to={"/"}><span><MdOutlineSettings /></span>Settings</Link>
+        <Link className="flex items-center gap-4" to={"/"}><span><MdHelpCenter /></span>Support</Link>
+        <Link className="flex items-center gap-4" to={"/"}><span><MdPrivacyTip /></span>Privacy</Link>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'primary': '#1293D5',
+      'secondary': '#FACD0D',
+      'gray': '#A4A19F',
+      'light': 'E3E9F0',
+      'black': '#000',
+      'white': '#fff'
+    }
   },
   plugins: [],
 };
